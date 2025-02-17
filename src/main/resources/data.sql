05bacee00daa882add65c0ceb46377216fea009a
Adones2005 <dadogar372@g.educaand.es>
2025-02-17 19:27:41 +0100

    Api segura

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -124,18 +124,18 @@ INSERT IGNORE INTO product_ticket (product_id, ticket_id) VALUES
 (4, 1),
 (5, 2);
 
--- Insertar datos de ejemplo para 'roles'
-INSERT IGNORE INTO roles (id, name) VALUES
-(1, 'ROLE_ADMIN'),
-(2, 'ROLE_MANAGER'),
-(3, 'ROLE_USER');
+        -- Insertar datos de ejemplo para 'roles'
+        INSERT IGNORE INTO roles (id, name) VALUES
+        (1, 'ROLE_ADMIN'),
+        (2, 'ROLE_MANAGER'),
+        (3, 'ROLE_USER');
 
 
--- Insertar datos de ejemplo para 'users'. La contraseña de cada usuario es password
-INSERT IGNORE INTO users (id, username, password, enabled, first_name, last_name, image, created_date, last_modified_date, last_password_change_date) VALUES
-(1, 'admin', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye', true, 'Admin', 'User', '/images/admin.jpg', NOW(), NOW(), NOW()),
-(2, 'manager', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye', true, 'Manager', 'User', '/images/manager.jpg', NOW(), NOW(), NOW()),
-(3, 'normal', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye', true, 'Regular', 'User', '/images/user.jpg', NOW(), NOW(), NOW());
+        -- Insertar datos de ejemplo para 'users'. La contraseña de cada usuario es password
+        INSERT IGNORE INTO users (id, username, password, enabled, first_name, last_name, image, created_date, last_modified_date, last_password_change_date) VALUES
+        (1, 'admin', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye', true, 'Admin', 'User', '/images/admin.jpg', NOW(), NOW(), NOW()),
+        (2, 'manager', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye', true, 'Manager', 'User', '/images/manager.jpg', NOW(), NOW(), NOW()),
+        (3, 'normal', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye', true, 'Regular', 'User', '/images/user.jpg', NOW(), NOW(), NOW());
 
 
 -- Asignar el rol de administrador al usuario con id 1
